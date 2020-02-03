e3996e60015c130c0e34cb3187d9deba86581aa1
samuelkyprianou <samuelkyprianou@gmail.com>
2020-02-03 17:25:48 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_song
+    self.artist.name + ' - ' + self.title
+  end
+
+  def artist_name
+    self.artist.name
+  end
+  
 end
